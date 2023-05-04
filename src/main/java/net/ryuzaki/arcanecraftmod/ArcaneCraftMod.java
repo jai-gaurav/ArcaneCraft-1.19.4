81ad1e52ba2ed5a43e1591b7b813eda58bc4e04b
Jai Gaurav <jaigaurav157@gmail.com>
2023-05-04 16:03:15 +0530

    added comment to test commits

--- src/main/java/net/ryuzaki/arcanecraftmod/ArcaneCraftMod.java
+++ src/main/java/net/ryuzaki/arcanecraftmod/ArcaneCraftMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// added a comment to test commits
 public class ArcaneCraftMod implements ModInitializer {
 	public static final String MOD_ID = "arcanecraftmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
