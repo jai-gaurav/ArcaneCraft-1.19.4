e279539538f30dde4f2296623001f6f3dfe05918
Jai Gaurav <jaigaurav157@gmail.com>
2023-05-04 16:01:18 +0530

    Initial commit

---
+++ src/main/java/net/ryuzaki/arcanecraftmod/ArcaneCraftMod.java
@@ -0,0 +1,14 @@
+package net.ryuzaki.arcanecraftmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArcaneCraftMod implements ModInitializer {
+	public static final String MOD_ID = "arcanecraftmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/ryuzaki/arcanecraftmod/ArcaneCraftModClient.java
@@ -0,0 +1,10 @@
+package net.ryuzaki.arcanecraftmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcaneCraftModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
